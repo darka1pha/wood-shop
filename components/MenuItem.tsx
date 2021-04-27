148e396b78fa693bbdb7ca9faefb1c8210e2ec00
DarkAlpha <abolfazl.omrani1999@gmail.com>
2021-04-27 17:09:24 +0430

    Add Menu Item

---
+++ components/MenuItem.tsx
@@ -0,0 +1,41 @@
+import {
+	Flex,
+	Icon,
+	IconButton,
+
+} from "@chakra-ui/react";
+import { BiChair } from "react-icons/bi";
+import Text from "./Text";
+
+interface MenuItem {
+	text: string;
+}
+
+const MenuItem = ({ text }: MenuItem) => {
+	return (
+		<Flex
+			p="0 1rem"
+			color="white"
+			direction="row-reverse"
+			alignItems="center"
+			borderLeft="0.5px solid #FFFFFF "
+			justifyContent="center"
+			cursor="pointer"
+			minW="120px"
+		>
+
+			<Icon
+				as={BiChair}
+				color="white"
+				ml=".5rem"
+			/>
+			<Text
+				variant="normal"
+			>
+				{text}
+			</Text>
+		</Flex>
+	);
+}
+
+export default MenuItem;
