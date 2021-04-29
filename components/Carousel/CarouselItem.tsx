f94a067601aec089c711cda68695e0b67f4672da
DarkAlpha <abolfazl.omrani1999@gmail.com>
2021-04-29 13:45:52 +0430

    CarouselItem Edited

--- components/Carousel/CarouselItem.tsx
+++ components/Carousel/CarouselItem.tsx
@@ -1,22 +1,75 @@
-import { Box } from "@chakra-ui/layout";
+import Icon from "@chakra-ui/icon";
+import { Box, Flex } from "@chakra-ui/layout";
+import { useState } from "react";
+import { FiShoppingCart } from "react-icons/fi";
+import { BsHeart, BsHeartFill } from "react-icons/Bs"
+import Text from "../Text";
 
 interface CarouselItem {
-	bgColor?: string
+	background_image?: string;
+	price?: string;
+	name?: string;
 }
 
-const CarouselItem = ({ bgColor }) => {
+const CarouselItem = ({ background_image, name, price }: CarouselItem) => {
+	const [isLiked, setIsLiked] = useState(false)
+	const bgImage = background_image ? background_image : "https://images.unsplash.com/photo-1533090481720-856c6e3c1fdc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHRhYmxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+
 	return (
-		<Box
-			bgColor={bgColor}
-			h="400px"
-			w="280px"
-			m="1rem 1rem"
-			// backgroundImage={`url(${bgImg})`}
-			// backgroundSize="cover"
+
+		<Flex
 			borderRadius=".5rem"
+			overflow="hidden"
+			flexDir="column"
+			h="360px"
+			w="240px"
+			m="1rem 1rem"
 			cursor="pointer"
+			boxShadow="md"
 		>
-		</Box>
+			<Flex
+				h="75%"
+				w="100%"
+				cursor="pointer"
+				bgImage={`url(${bgImage})`}
+				bgRepeat="no-repeat"
+				bgSize="cover"
+				borderRadius=".5rem .5rem 0 0 / .5rem .5rem 0 0"
+			/>
+			<Flex
+				flexDir="column"
+				padding=".5rem"
+				justifyContent="center"
+				alignItems="center"
+			>
+				<Text
+					m=".1rem"
+					color="black"
+					variant="normal"
+					dir="rtl"
+				>
+					میز چهارپایه MDF
+				</Text>
+				<Text m=".1rem" color="black" variant="normalThin">
+					750.000
+				</Text>
+				<Flex
+					w="25%"
+					m=".1rem"
+					justifyContent="space-evenly"
+				>
+					<Icon
+						as={FiShoppingCart}
+						color="black"
+					/>
+					<Icon
+						onClick={() => setIsLiked(!isLiked)}
+						as={!isLiked ? BsHeart : BsHeartFill}
+						color="red"
+					/>
+				</Flex>
+			</Flex>
+		</Flex>
 	);
 }
 
