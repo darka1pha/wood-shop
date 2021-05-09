c45ab93e682df49bce2b50f525f5c91b03baf5d0
DarkAlpha <abolfazl.omrani1999@gmail.com>
2021-05-09 10:15:05 +0430

    Category Menu Added

--- components/Category/CategoryMenu.tsx
+++ components/Category/CategoryMenu.tsx
@@ -1,55 +1,32 @@
 import {
-	Accordion,
-	AccordionItem,
-	AccordionButton,
-	AccordionPanel,
-	AccordionIcon,
-	Flex,
-	Box,
+	Accordion
 } from "@chakra-ui/react"
+import CategoryItem from "./CategoryItem"
 
 
 const CategoryMenu = () => {
+	const testItems = ["ایتم 1", "ایتم 2"]
 	return (
-		<Flex
-
+		<Accordion
+			borderRadius=".5rem"
+			overflow="hidden"
+			w="100%"
+			defaultIndex={[0]}
 		>
-			<Accordion defaultIndex={[0]} allowMultiple>
-				<AccordionItem>
-					<h2>
-						<AccordionButton>
-							<Box flex="1" textAlign="left">
-								Section 1 title
-        			</Box>
-							<AccordionIcon />
-						</AccordionButton>
-					</h2>
-					<AccordionPanel pb={4}>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-						veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-						commodo consequat.
-    </AccordionPanel>
-				</AccordionItem>
+			<CategoryItem
+				items={testItems}
+				title="صندلی"
+			/>
+			<CategoryItem
+				items={testItems}
+				title="میز"
+			/>
+			<CategoryItem
+				items={testItems}
+				title="قفسه"
+			/>
 
-				<AccordionItem>
-					<h2>
-						<AccordionButton>
-							<Box flex="1" textAlign="left">
-								Section 2 title
-        			</Box>
-							<AccordionIcon />
-						</AccordionButton>
-					</h2>
-					<AccordionPanel pb={4}>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-						veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-						commodo consequat.
-    </AccordionPanel>
-				</AccordionItem>
-			</Accordion>
-		</Flex>
+		</Accordion>
 	);
 }
 
