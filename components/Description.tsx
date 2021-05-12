eaf71d79b3706e0e9775f75913a5f95ba645f7a1
DarkAlpha <abolfazl.omrani1999@gmail.com>
2021-05-12 11:47:30 +0430

    Background Fixed

--- components/Description.tsx
+++ components/Description.tsx
@@ -5,26 +5,26 @@ const Description = () => {
 	return (
 		<Flex
 			w="100%"
-			h={{base:"100vh",md:"480px"}}
+			h={{ base: "100vh", md: "480px" }}
 			padding="2rem"
 			justifyContent="space-evenly"
 			bgImage="
 			linear-gradient(to left, rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3) ),
 			url('/assets/description.webp')"
 			bgSize="cover"
-			bgAttachment="fixed"
+			bgAttachment={{ base: "unset", md: "fixed" }}
 			bgRepeat="no-repeat"
 			flexDir={{ base: "column-reverse", md: "row" }}
 			className="blur"
 		>
 			<Flex
-				w={{base:"100%",md:"65%"}}
+				w={{ base: "100%", md: "65%" }}
 				h="100%"
 				borderRadius=".5rem"
 				className="glass"
 			/>
 			<Flex
-				w={{base:"100%",md:"30%"}}
+				w={{ base: "100%", md: "30%" }}
 				h="100%"
 				ml=".5rem"
 				justifyContent="center"
