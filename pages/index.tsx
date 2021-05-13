5faa0c95267ca0d44bff65d69fc8c82c14870137
DarkAlpha <abolfazl.omrani1999@gmail.com>
2021-05-13 14:22:25 +0430

    Updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ export default function Home() {
       bgColor="bgColor"
       minH="100vh"
       overflowX="hidden"
+      pt={{ base: 0, md: "85px" }}
     >
       <Head>
         <title>Create Next App</title>
