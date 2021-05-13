be5c8f0dfa8beeb3560cea182fb3cb9c1bc636e8
DarkAlpha <abolfazl.omrani1999@gmail.com>
2021-05-13 14:21:39 +0430

    Add Filter Styles / Mobile

--- components/Filter/Filter.tsx
+++ components/Filter/Filter.tsx
@@ -1,18 +1,31 @@
+import { Button } from "@chakra-ui/button";
 import { Flex } from "@chakra-ui/layout";
 import { useState } from "react";
 import Text from "../Text";
 import FilterItem from "./FilterItem";
 
-const Filter = () => {
+import { HiSortAscending } from "react-icons/hi"
+import Icon from "@chakra-ui/icon";
+import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay } from "@chakra-ui/modal";
+import { useDisclosure } from "@chakra-ui/hooks";
+import { Radio, RadioGroup, useRadioGroup } from "@chakra-ui/radio";
+import RadioCard from "../RadioCard";
 
+const Filter = () => {
+	const { isOpen, onOpen, onClose } = useDisclosure()
 	const [active, setActive] = useState("پیشفرض")
+	const { getRadioProps } = useRadioGroup({
+		name: "Ordering",
+		defaultValue: "پیشفرض",
+	})
+	const options = ["پیشفرض", "جدیدترین", "گرانترین", "ارزانترین", "پرفروشترین"]
 
 	return (
 		<Flex
 			h="45px"
-			bgColor="itemsBg"
+			bgColor={{ base: "transparent", md: "itemsBg" }}
 			alignItems="center"
-			p={{ base: "0 .5rem 0 .5rem", md: "0 2rem 0 1rem" }}
+			p={{ base: "0 .5rem 0 0", md: "0 2rem 0 1rem" }}
 			dir="rtl"
 			borderRadius=".5rem"
 			mb="1rem"
@@ -20,17 +33,75 @@ const Filter = () => {
 			<Text
 				variant={"heading7"}
 				fontSize={{ base: "10px", md: "12px" }}
+				display={{ base: "none", md: "block" }}
 			>
 				مرتب کردن بر اساس
       </Text>
 			<Flex
-				mr={{base:"1rem",md:"2rem"}}
+				mr={{ base: "0", md: "2rem" }}
+				display={{ base: "none", md: "flex" }}
 			>
 				<FilterItem onClick={(e: any) => setActive("پیشفرض")} isActive={active === "پیشفرض"} type="پیشفرض" />
 				<FilterItem onClick={(e: any) => setActive("جدیدترین")} isActive={active === "جدیدترین"} type="جدیدترین" />
 				<FilterItem onClick={(e: any) => setActive("گرانترین")} isActive={active === "گرانترین"} type="گرانترین" />
 				<FilterItem onClick={(e: any) => setActive("ارزانترین")} isActive={active === "ارزانترین"} type="ارزانترین" />
 				<FilterItem onClick={(e: any) => setActive("پرفروشترین")} isActive={active === "پرفروشترین"} type="پرفروشترین" />
+			</Flex>
+			<Flex
+				display={{ base: "flex", md: "none" }}
+			>
+				<Button
+					_focus={{
+						outline: 0
+					}}
+					_active={{
+						outline: 0
+					}}
+					_hover={{
+						outline: 0
+					}}
+					onClick={onOpen}
+					fontFamily="iranSans"
+					fontSize="12px"
+					display="flex"
+					bgColor="itemsBorder"
+					h="35px"
+					rightIcon={<Icon fontSize="1rem" as={HiSortAscending} color="black" />}
+				>
+					مرتب سازی
+				</Button>
+				<Modal
+					isOpen={isOpen}
+					onClose={onClose}>
+					<ModalOverlay />
+					<ModalContent h="480px" w="320px">
+						<ModalCloseButton
+							_focus={{
+								outline: 0
+							}}
+							_active={{
+								outline: 0
+							}}
+						/>
+						<ModalBody >
+							<ModalHeader
+								fontFamily="iranSans"
+								fontSize="18px"
+								display="flex"
+								alignItems="center"
+								justifyContent="center"
+							>
+								مرتب سازی بر اساس
+						</ModalHeader>
+							{options.map((value, key) => {
+								const radio = getRadioProps({ value })
+								return (
+									<RadioCard onClick={() => console.log(value)} lable={value} key={key} radio={radio} />
+								)
+							})}
+						</ModalBody>
+					</ModalContent>
+				</Modal>
 			</Flex>
 		</Flex>
 	);
