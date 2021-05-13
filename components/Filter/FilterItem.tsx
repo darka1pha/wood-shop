013ce9d106d227be2ee9ad83c05ff933fe88bd6d
DarkAlpha <abolfazl.omrani1999@gmail.com>
2021-05-13 14:22:04 +0430

    Add Filter Styles / Mobile

--- components/Filter/FilterItem.tsx
+++ components/Filter/FilterItem.tsx
@@ -9,9 +9,8 @@ interface FilterItem {
 const FilterItem = ({ type, isActive, onClick }: FilterItem) => {
 	return (
 		<Text
-			m={{ base: "0 .5rem 0 .5rem", md: "0 1rem 0 1rem" }}
+			m="0 1rem 0 1rem"
 			variant="normal"
-			fontSize={{ base: "10px", md: "12px" }}
 			color={isActive ? "primary" : "black"}
 			cursor="pointer"
 			fontWeight={isActive ? "semibold" : "normal"}
