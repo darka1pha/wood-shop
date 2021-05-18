8c5eb185b922d1e9368f899d4cdefae88c01799e
DarkAlpha <abolfazl.omrani1999@gmail.com>
2021-05-18 09:47:33 +0430

    Updated

--- components/Description.tsx
+++ components/Description.tsx
@@ -7,37 +7,43 @@ const Description = () => {
 			w="100%"
 			h={{ base: "100vh", md: "480px" }}
 			padding="2rem"
-			justifyContent="space-evenly"
 			bgImage="
 			linear-gradient(to left, rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3) ),
 			url('/assets/description.webp')"
 			bgSize="cover"
 			bgAttachment={{ base: "unset", md: "fixed" }}
 			bgRepeat="no-repeat"
-			flexDir={{ base: "column-reverse", md: "row" }}
+			justifyContent="center"
 			className="blur"
 		>
 			<Flex
-				w={{ base: "100%", md: "65%" }}
-				h="100%"
-				borderRadius=".5rem"
-				className="glass"
-			/>
-			<Flex
-				w={{ base: "100%", md: "30%" }}
-				h="100%"
-				ml=".5rem"
-				justifyContent="center"
-				alignItems="center"
+				justifyContent="space-evenly"
+				flexDir={{ base: "column-reverse", md: "row" }}
+				w="100%"
+				maxW="1280px"
 			>
-				<Text
-					textAlign="center"
-					variant="heading3"
-					color="white"
-					dir="rtl"
+				<Flex
+					w={{ base: "100%", md: "65%" }}
+					h="100%"
+					borderRadius=".5rem"
+					className="glass"
+				/>
+				<Flex
+					w={{ base: "100%", md: "30%" }}
+					h="100%"
+					ml=".5rem"
+					justifyContent="center"
+					alignItems="center"
 				>
-					تولیدی چوب و دکوراسیون داخلی فرحبخش
+					<Text
+						textAlign="center"
+						variant="heading3"
+						color="white"
+						dir="rtl"
+					>
+						تولیدی چوب و دکوراسیون داخلی فرحبخش
 				</Text>
+				</Flex>
 			</Flex>
 		</Flex>
 	);
