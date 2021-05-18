309f133d31061a1f4b82fcfe226a57096bee3949
DarkAlpha <abolfazl.omrani1999@gmail.com>
2021-05-18 09:48:23 +0430

    Updated

--- components/RadioCard.tsx
+++ components/RadioCard.tsx
@@ -3,46 +3,62 @@ import { useRadio } from "@chakra-ui/radio"
 
 
 interface IRadioCard {
-    lable?: string;
-    radio?: any;
-    onClick?: () => void;
+	lable?: string;
+	radio?: any;
+	onClick?: () => void;
+	height?: string;
+	width?: string;
+	borderRadius?: string;
+	image_url?: string;
+	use_as_color_pallete?: boolean;
 }
 
-const RadioCard = ({ lable, radio, onClick }: IRadioCard) => {
-    const { getInputProps, getCheckboxProps } = useRadio(radio)
+const RadioCard = ({
+	lable,
+	radio,
+	onClick,
+	width,
+	borderRadius,
+	height,
+	image_url,
+	use_as_color_pallete
+}: IRadioCard) => {
+	const { getInputProps, getCheckboxProps } = useRadio(radio)
+	const input = getInputProps()
+	const checkbox = getCheckboxProps()
 
-    const input = getInputProps()
-    const checkbox = getCheckboxProps()
-
-    return (
-        <Box as="label">
-            <input {...input} />
-            <Box
-                m="1rem"
-                {...checkbox}
-                cursor="pointer"
-                borderWidth="1px"
-                borderRadius="md"
-                boxShadow="md"
-                fontFamily="iranSans"
-                textAlign="center"
-                _checked={{
-                    bg: "red.500",
-                    color: "white",
-                    borderColor: "red.500",
-                }}
-                _focus={{
-                    boxShadow: 0,
-                    outline: 0
-                }}
-                px={5}
-                py={3}
-                onClick={onClick}
-            >
-                {lable}
-            </Box>
-        </Box>
-    )
+	return (
+		<Box as="label">
+			<input {...input} />
+			<Box
+				m="1rem"
+				{...checkbox}
+				cursor="pointer"
+				borderWidth={use_as_color_pallete ? "3px" : "1px"}
+				borderRadius={use_as_color_pallete ? borderRadius : "md"}
+				boxShadow="md"
+				fontFamily="iranSans"
+				textAlign="center"
+				h={use_as_color_pallete ? height : "auto"}
+				w={use_as_color_pallete ? width : "auto"}
+				bg={use_as_color_pallete ? "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStnZu3mIESjUwsAQSa25-GFsj_IGAUW6mLCQ&usqp=CAU')" : ""}
+				_checked={{
+					bg: use_as_color_pallete ? null : "red.500",
+					color: "white",
+					borderColor: "red.500",
+				}}
+				_focus={{
+					boxShadow: 0,
+					outline: 0
+				}}
+				px={3}
+				py={3}
+				onClick={onClick}
+			>
+				{lable}
+			</Box>
+		</Box>
+	)
 }
 
 export default RadioCard
