f945edb4e8fcd6955e97f51c080dc3a16ee08adc
DarkAlpha <abolfazl.omrani1999@gmail.com>
2021-05-31 16:46:15 +0430

    add api pathes

---
+++ API/apiPathes.tsx
@@ -0,0 +1,11 @@
+export const apiPathes = {
+  MAIN:"http://192.168.1.178:8000/api/v0/" ,
+  AUTH:"accounts/" ,
+  UPDATE_PROFILE:"profile/update" ,
+  RESET_VERIFY:"reset/verify" ,
+  SIGN_IN_PASSWORD:"signin/password" ,
+  SIGN_IN_PHONENUMBER:"signin/phonenumber" ,
+  SIGN_IN_VERIFY:"signin/verify" ,
+  SIGN_UP:"signup/" ,
+  SIGN_UP_VERIFY:"verify"
+}
