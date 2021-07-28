86a2dd4e0831afbecbe6938d4898dbf72afd2292
DarkAlpha <abolfazl.omrani1999@gmail.com>
2021-07-28 07:39:07 +0430

    Update Menu

--- components/Menu/MenuItem.tsx
+++ components/Menu/MenuItem.tsx
@@ -1,51 +1,99 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import { ISetCurrentCategory, setCurrentCategory } from "../../redux";
 import Text from "../Text";
 
 import { useRouter } from "next/router";
 import { connect } from "react-redux";
+import { Dispatch, useEffect } from "react";
 
 interface MenuItem {
   text: string;
   id: number;
   setCurrentCategory: ({ name, id }: ISetCurrentCategory) => void;
+  category_set: Array<any>;
+  setIsMenuOpen: Dispatch<boolean>
 }
 
-const MenuItem = ({ text, id, setCurrentCategory }: MenuItem) => {
+const MenuItem = ({ text, id, setCurrentCategory, category_set, setIsMenuOpen }: MenuItem) => {
   const router = useRouter();
+  useEffect(() => {
+    console.log(category_set)
+  }, [])
   return (
-    <Button
-      p="0 1rem"
-      color="white"
-      dir="rtl"
-      border="none"
-      display="flex"
-      borderRadius="0"
-      alignItems="center"
-      borderLeft="0.5px solid #FFFFFF"
-      justifyContent="flex-start"
-      _hover={{
-        bg: "transparent",
-      }}
-      _active={{
-        bg: "transparent",
-      }}
-      _focus={{
-        outline: "none",
-      }}
-      variant="ghost"
-      onClick={() => {
-        router.push({
-          pathname: "/[category]",
-          query: {
-            category: text,
-            order: "default"
-          },
-        });
-        setCurrentCategory({ name: text, id });
-      }}>
-      <Text variant="normal">{text}</Text>
-    </Button>
+    <Flex minW="80px" m="0.5rem 0" flexDir="column">
+      <Button
+        p="0 1rem"
+        color="white"
+        dir="rtl"
+        border="none"
+        display="flex"
+        borderRadius="0"
+        alignItems="center"
+        justifyContent="flex-start"
+        _hover={{
+          bg: "transparent",
+        }}
+        _active={{
+          bg: "transparent",
+        }}
+        _focus={{
+          outline: "none",
+        }}
+        variant="ghost"
+        onClick={() => {
+          setIsMenuOpen(false)
+          router.push({
+            pathname: "/[category]",
+            query: {
+              category: text,
+              order: "default"
+            },
+          });
+          setCurrentCategory({ name: text, id });
+        }}>
+        <Text color="black" variant="heading6">{text}</Text>
+      </Button>
+      <Flex flexDir="column">
+        {
+          category_set.map(({ title, id }, key) => (
+            <Button
+              key={key}
+              p="0 1rem"
+              color="white"
+              dir="rtl"
+              border="none"
+              display="flex"
+              borderRadius="0"
+              alignItems="center"
+              borderLeft="0.5px solid #FFFFFF"
+              justifyContent="flex-start"
+              _hover={{
+                bg: "transparent",
+              }}
+              _active={{
+                bg: "transparent",
+              }}
+              _focus={{
+                outline: "none",
+              }}
+              variant="ghost"
+              onClick={() => {
+                setIsMenuOpen(false)
+                router.push({
+                  pathname: "/[category]",
+                  query: {
+                    category: text,
+                    order: "default"
+                  },
+                });
+                setCurrentCategory({ name: text, id });
+              }}>
+              <Text color="#4a4a4a" variant="normalMedium">{title}</Text>
+            </Button>
+          ))
+        }
+      </Flex>
+    </Flex>
   );
 };
 
--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -1,13 +1,19 @@
-import { useEffect, useState } from "react";
-import MenuItem from "./MenuItem";
-import { motion } from "framer-motion";
+import { MouseEventHandler, useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import { Box } from "@chakra-ui/layout";
 import { useGetCategories } from "../../API";
 import { useQueryClient } from "react-query";
 import LandingSkeleton from "../Skeleton/LandingSkeleton";
+import { MenuButton, Menu as CMenu, MenuList, MenuGroup, BoxProps, Icon, Flex, FlexProps } from "@chakra-ui/react";
+import MenuItem from "./MenuItem";
+import Text from "../Text";
+import { FiShoppingBag } from "react-icons/fi";
 
 const Menu = () => {
   const queryClient = useQueryClient()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const MotionFlex = motion<FlexProps>(Flex)
+
   // const [categories, setCategories] = useState(null)
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
@@ -18,10 +24,47 @@ const Menu = () => {
     setPrevScrollPos(currentScrollPos);
   };
 
+  const variants = {
+    visible: {
+      x: "-.5rem",
+      display: "flex",
+      opacity: 1,
+      transition: {
+        type: "tween",
+        ease: "easeInOut",
+        duration: 0.4,
+        display: {
+          delay: 0.4,
+          duration: 0.2
+        },
+      }
+    },
+    hidden: {
+      x: "100px",
+      display: "none",
+      opacity: 0,
+      transition: {
+        type: "tween",
+        ease: "easeInOut",
+        duration: 0.4,
+        display: {
+          delay: 0.4,
+          duration: 0.2
+        },
+      }
+    }
+  }
+
+
+
+
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     // setCategories(queryClient.getQueryData([`categories`]))
-    return () => window.removeEventListener("scroll", handleScroll);
+    console.log("Cats: ", categories)
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
   }, [prevScrollPos, visible, handleScroll]);
 
   return (
@@ -29,6 +72,7 @@ const Menu = () => {
       <motion.div
         layout
         animate={{
+          height: visible ? "60px" : "0",
           top: visible ? "85px" : "-10px",
           opacity: visible ? 1 : 0,
         }}
@@ -37,7 +81,7 @@ const Menu = () => {
           delay: 0.02,
         }}
         style={{
-          padding: "4px 3px 8px 3px",
+          // padding: "4px 3px 12px 4px",
           width: "100%",
           zIndex: 3,
           position: "fixed",
@@ -47,9 +91,33 @@ const Menu = () => {
           backgroundColor: "#42301e",
           flexDirection: "row-reverse",
         }}>
-        {categories?.map(({ title, id }, key: number) => (
-          <MenuItem id={id} text={title} key={key} />
-        ))}
+
+        <Box
+          onMouseOutCapture={() => setIsMenuOpen(false)}
+          onMouseOver={() => setIsMenuOpen(true)}
+          pos="relative"
+        >
+          <Flex alignItems="center" mr="1rem" cursor="pointer">
+            <Text color="white" variant="heading6">دسته بندی ها</Text>
+            <Icon ml="0.2rem" color="white" as={FiShoppingBag} h="20px" w="20px" />
+          </Flex>
+          <motion.div
+            animate={isMenuOpen ? "visible" : "hidden"}
+            variants={variants}
+            style={{
+              borderRadius: "0.5rem",
+              top: "2rem",
+              right: "1rem",
+              position: "absolute",
+              backgroundColor: "white",
+              direction: "rtl"
+            }}
+          >
+            {categories?.map(({ title, id, category_set }, key: number) => (
+              <MenuItem setIsMenuOpen={setIsMenuOpen} id={id} text={title} key={key} category_set={category_set} />
+            ))}
+          </motion.div>
+        </Box>
       </motion.div>
     </Box>
   );
